d4eb569778a2123bfd7f828ccfe5bbfbe9530b7b
abdallah chamakh <chamakhabdallah8@gmail.com>
2025-01-30 14:12:11 +0100

    new commit

---
+++ src/Apps/Settings/Controllers/SettingsController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Dolzay\Apps\Settings\Controllers;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Dolzay\CustomClasses\Db\DzDb ;  
+use Dolzay\Apps\Settings\Entities\Settings ;
+use Dolzay\Apps\Settings\Entities\Carrier ;
+
+
+
+class SettingsController extends FrameworkBundleAdminController
+{
+
+
+
+    public function getSettings(Request $request)
+    { 
+
+        $db = DzDb::getInstance();
+        $db->beginTransaction();
+
+        // get the settings
+        $stmt = $db->query("SELECT license_type,
+                                   expiration_date,
+                                   post_submit_state_id FROM ".Settings::TABLE_NAME." LIMIT 1");
+        $settings = $stmt->fetch();
+        $defaultLanguageId = $this->getContext()->language->id;
+
+        // get the order state options
+        $stmt = $db->query("SELECT id_order_state,name FROM "._DB_PREFIX_."order_state_lang WHERE id_lang=".$defaultLanguageId);
+        $order_state_options = $stmt->fetchAll();
+
+        // get the carriers
+        $stmt = $db->query("SELECT name,logo FROM ".Carrier::TABLE_NAME);
+        $carriers = $stmt->fetchAll();
+
+        $db->commit();
+
+        // set the carriers and order state options in the settings
+        $settings['carriers'] = $carriers;
+        $settings['order_state_options'] = $order_state_options;
+
+        return $this->render('@Modules/dolzay/views/templates/admin/settings/settings.html.twig',[
+            'settings'=>$settings,
+            'carriers'=>$carriers,
+            'order_state_options'=> $order_state_options
+        ]);
+    }
+
+    public function updateSettings(Request $request)
+    {
+        $db = DzDb::getInstance();
+        $db->beginTransaction();
+
+        $order_post_submit_state_id = $request->get('order_post_submit_state_id');
+
+        // update the settings
+        $db = DzDb::getInstance();
+        $stmt = $db->prepare("UPDATE ".Settings::TABLE_NAME." SET post_submit_state_id = :post_submit_state_id");
+        $stmt->bindParam(':post_submit_state_id', $order_post_submit_state_id, \PDO::PARAM_INT);
+        $stmt->execute();
+
+        return new JsonResponse(['status'=>"success",'message' => 'Settings updated successfully']);
+    }
+}
